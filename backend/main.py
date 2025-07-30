2dcac1cc034708c2647e70de53d3e464471c4cff
Louis Camborieux <camborieuxl@gmail.com>
2025-07-30 16:48:45 +0200

    feat(getProduct) added filtering Added http functionnal tests

--- backend/main.py
+++ backend/main.py
@@ -10,16 +10,17 @@ async def root():
     return  {"message": "Hello World"}
 
 @app.get("/products")
-async def productsGet():
+async def productsGet(category : str = None):
     try :
         with open(PRODUCTS_FILE) as products:
             prodData = json.load(products)
-            return json.load(products)
-    except :
-            raise HTTPException(status_code=500, detail="error getting data") 
-    return [
-            {"id" : 0, 
-             "name" : "productName",
-             "category" : "test"}
-        ]
+            return list(filter(lambda product : 
+                            (category == None or product["category"] == category)
+                            ,prodData
+                            )
+                        )
+            # return json.load(products)
+    except Exception as e :
+            print ("error:", str(e))
+            raise HTTPException(status_code=500, detail="error getting data: "+ str(e)) 
     
